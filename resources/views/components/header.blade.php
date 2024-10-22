fcf0dfad33e50b195205f86dc056f8fd40c35a62
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-10-22 21:17:38 +0700

    add header

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,10 @@
+<div class="container mx-auto p-4">
+    <div class="flex justify-between items-center mb-4">
+        <h1 class="text-2xl font-bold">
+            List Stok Obat
+        </h1>
+        <img alt="User avatar" class="rounded-full" height="40"
+            src="https://storage.googleapis.com/a1aa/image/eXgLgHiBK4XOaSeReHbBTgKvPJfJ5Nqat8pZXPwt0MuC52iOB.jpg"
+            width="40" />
+    </div>
+</div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,15 +1,21 @@
 <!doctype html>
 <html>
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>{{ $title ?? "SIMBAT" }}</title>
-  @vite('resources/css/app.css')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title ?? 'SIMBAT' }}</title>
+    @vite('resources/css/app.css')
 </head>
+
 <body>
-        @include('components.sidebar')
+    @include('components.sidebar')
+    <div class="sm:ml-64">
+        @include('components.header')
+    </div>
     <div class="p-4 sm:ml-64">
         @yield('container')
     </div>
 </body>
+
 </html>
