7e8d8e695a8ced93bcf5759953b25c674935cee7
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-10-23 03:00:27 +0700

    [DONE] Report pages simple view

--- resources/views/pages/report/drug.blade.php
+++ resources/views/pages/report/drug.blade.php
@@ -1,4 +1,64 @@
 @extends('layouts.main')
 @section('container')
+<form action="" class="flex flex-row justify-between w-max gap-3">
+    <input class="ring-2 ring-gray-500 py-1 px-2 rounded-sm" type="date" name="" id="">
+    <h1>sampai</h1>
+    <input class="ring-2 ring-gray-500 py-1 px-2 rounded-sm" type="date" name="" id="">
+    <button class="bg-indigo px-2 py-1 rounded-full text-white font-bold text-xs hover:bg-indigo-800" type="submit">APPLY</button>
+</form>
+<div class="flex justify-end">
+    <form action="">
+        <input type="text" name="" id="" placeholder="Search..." class="ring-2 ring-gray-300 rounded-full px-6 py-2">
+    </form>
+</div>
+<div class="shadow-lg mt-8 p-4 rounded-md">
+    <table class="w-full">
+        <thead class="bg-gray-200 text-sm">
+            <th class="py-4 rounded-l-sm">NO</th>
+            <th class="py-4">KODE OBAT</th>
+            <th class="py-4">NAMA OBAT</th>
+            <th class="py-4">STOK KONVERSI</th>
+            <th class="py-4">EXP TERDEKAT</th>
+            <th class="py-4">STATUS</th>
+            <th class="py-4 rounded-r-sm">ACTION</th>
+        </thead>
+        <tbody>
+            @for ($i = 0; $i < 10; $i++)
+            <tr>
+                <td class="text-center py-3">1</td>
+                <td class="text-center py-3">#AAA111</td>
+                <td class=" py-3">Paracetamol Gatau Apa Merk nya</td>
+                <td class="text-center py-3">70 Box</td>
+                <td class="text-center py-3">20 Januari 2024</td>
+                <td class="text-center py-3">-</td>
+                <td class="flex justify-center py-3"><a href="{{ route('report.drugs.show',1) }}" class="bg-lavender-200 hover:bg-lavender p-1 rounded-md"><svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M9.99972 2.5C14.4931 2.5 18.2314 5.73333 19.0156 10C18.2322 14.2667 14.4931 17.5 9.99972 17.5C5.50639 17.5 1.76805 14.2667 0.983887 10C1.76722 5.73333 5.50639 2.5 9.99972 2.5ZM9.99972 15.8333C11.6993 15.833 13.3484 15.2557 14.6771 14.196C16.0058 13.1363 16.9355 11.6569 17.3139 10C16.9341 8.34442 16.0038 6.86667 14.6752 5.80835C13.3466 4.75004 11.6983 4.17377 9.99972 4.17377C8.30113 4.17377 6.65279 4.75004 5.3242 5.80835C3.9956 6.86667 3.06536 8.34442 2.68555 10C3.06397 11.6569 3.99361 13.1363 5.32234 14.196C6.65106 15.2557 8.30016 15.833 9.99972 15.8333V15.8333ZM9.99972 13.75C9.00516 13.75 8.05133 13.3549 7.34807 12.6516C6.64481 11.9484 6.24972 10.9946 6.24972 10C6.24972 9.00544 6.64481 8.05161 7.34807 7.34835C8.05133 6.64509 9.00516 6.25 9.99972 6.25C10.9943 6.25 11.9481 6.64509 12.6514 7.34835C13.3546 8.05161 13.7497 9.00544 13.7497 10C13.7497 10.9946 13.3546 11.9484 12.6514 12.6516C11.9481 13.3549 10.9943 13.75 9.99972 13.75ZM9.99972 12.0833C10.5523 12.0833 11.0822 11.8638 11.4729 11.4731C11.8636 11.0824 12.0831 10.5525 12.0831 10C12.0831 9.44747 11.8636 8.91756 11.4729 8.52686C11.0822 8.13616 10.5523 7.91667 9.99972 7.91667C9.44719 7.91667 8.91728 8.13616 8.52658 8.52686C8.13588 8.91756 7.91639 9.44747 7.91639 10C7.91639 10.5525 8.13588 11.0824 8.52658 11.4731C8.91728 11.8638 9.44719 12.0833 9.99972 12.0833Z" fill="black"/>
+                    </svg>
+                    </a></td>
+            </tr>
+                
+            @endfor
+        </tbody>
+    </table>
+    <div class="flex justify-between items-center">
+        <a href="" class="bg-bright shadow-md rounded-md p-2 text-xs ml-4">CETAK</a>
+        <div class="flex justify-end items-center mt-4 gap-4">
+            <div class="text-sm">Showing 1 to 10 of 50 entries</div>
+                <!-- Pagination -->
+                <div class="flex justify-end">
+                    <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-l-md hover:bg-gray-100">1</a>
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">2</a>
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">3</a>
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">...</a>
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">9</a>
+                        <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-r-md hover:bg-gray-100">10</a>
+                    </nav>
+                </div>
+            </div>
+        </div>
 
+    </div>
+
+</div>
 @endsection
--- resources/views/pages/report/drugDetail.blade.php
+++ resources/views/pages/report/drugDetail.blade.php
@@ -1,4 +1,187 @@
 @extends('layouts.main')
 @section('container')
-    
+    <div class="bg-white shadow-md rounded-lg mb-6">
+        <div class="bg-gray-200 p-4 rounded-t-lg">
+            <h2 class="font-semibold">Detail Obat</h2>
+        </div>
+        <div class="p-4">
+            <table class="w-full">
+                <tbody>
+                    <tr class="border-b">
+                        <td class="py-2">Nama</td>
+                        <td class="py-2">NULL</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2">Kode Obat</td>
+                        <td class="py-2">#abc111</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2">Jenis</td>
+                        <td class="py-2">NULL</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2">Kategori</td>
+                        <td class="py-2">NULL</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2">Produsen</td>
+                        <td class="py-2">NULL</td>
+                    </tr>
+                    <tr class="border-b">
+                        <td class="py-2">Vendor</td>
+                        <td class="py-2">NULL</td>
+                    </tr>
+                    <tr>
+                        <td class="py-2">Sisa</td>
+                        <td class="py-2">123</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <h2 class="text-xl font-semibold mb-4">STOK OBAT</h2>
+    <div class="bg-white shadow-md rounded-lg overflow-hidden p-6">
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr class="bg-gray-200 text-black uppercase text-sm leading-normal">
+                    <th class="border p-2">No</th>
+                    <th class="border p-2">Jenis Packaging Obat</th>
+                    <th class="border p-2">Margin</th>
+                    <th class="border p-2">Stok konversi</th>
+                    <th class="border p-2">Harga Jual</th>
+                </tr>
+            </thead>
+            <tbody class="text-gray-700 text-sm font-light">
+                @for ($i = 1; $i < 8; $i++)
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                    <td class="py-3 px-6 text-center">{{ $i }}</td>
+                    <td class="py-3 px-6 text-center">Antibiotik (100ml)</td>
+                    <td class="py-3 px-6 text-center">15%</td>
+                    <td class="py-3 px-6 text-center">100</td>
+                    <td class="py-3 px-6 text-center">15.000</td>
+                </tr>
+                    
+                @endfor
+            </tbody>
+        </table>
+        <div class="flex justify-end items-center mt-4 gap-4">
+            <div class="text-sm">Showing 1 to 10 of 50 entries</div>
+            <div class="flex justify-end">
+                <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-l-md hover:bg-gray-100">1</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">2</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">3</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">...</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">9</a>
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-r-md hover:bg-gray-100">10</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <h2 class="text-xl font-semibold my-4">KATEGORI BERDASARKAN EXP DATE</h2>
+    <div class="bg-white shadow-md rounded-lg overflow-hidden p-6">
+        <table class="min-w-full leading-normal">
+            <thead>
+                <tr class="bg-gray-200 text-black uppercase text-sm leading-normal">
+                    <th class="border p-2">No</th>
+                    <th class="border p-2">Waktu Expired Obat</th>
+                    <th class="border p-2">Kuantiti</th>
+                    <th class="border p-2">Action</th>
+                </tr>
+            </thead>
+            <tbody class="text-gray-700 text-sm font-light">
+                @for ($i = 1; $i < 8; $i++)
+                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                    <td class="py-3 px-6 text-center">{{ $i }}</td>
+                    <td class="py-3 px-6 text-center">12 Desember 2024</td>
+                    <td class="py-3 px-6 text-center">150</td>
+                    <td class="py-3 px-6 text-center">
+                        <a href="{{ route('inventory.retur', 1) }}"
+                            class="bg-gold text-black text-sm px-2 py-1 rounded-lg shadow hover:bg-gold-700 transition-colors duration-200">Retur</a>
+                        <a href="{{ route('inventory.stocks.show', 1) }}"
+                            class="bg-pink text-black text-sm px-2 py-1 rounded-lg shadow hover:bg-pink-700 transition-colors duration-200">Buang</a>
+                    </td>
+                </tr>
+                    
+                @endfor
+            </tbody>
+        </table>
+        <div class="flex justify-end items-center mt-4 gap-4">
+            <div class="text-sm">Showing 1 to 10 of 50 entries</div>
+            <!-- Pagination -->
+            <div class="flex justify-end">
+                <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-l-md hover:bg-gray-100">1</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">2</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">3</a>
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">...</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">9</a>
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-r-md hover:bg-gray-100">10</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Tabel Histori Transaksi -->
+    <h2 class="text-xl font-semibold my-4">HISTORI TRANSAKSI</h2>
+    <div class="bg-white shadow-md rounded-lg overflow-hidden p-6">
+        <div style="overflow-x: auto;">
+            <table class="min-w-full leading-normal">
+                <thead>
+                    <tr class="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
+                        <th class="border p-2" rowspan="2">No</th>
+                        <th class="border p-2" rowspan="2">Jenis Packaging Obat</th>
+                        <th class="border p-2" rowspan="2">Margin</th>
+                        <th class="border p-2" rowspan="2">Harga Jual</th>
+                        <th class="border p-2" rowspan="2">Kuantiti</th>
+                        <th class="border p-2" colspan="3">Status</th>
+                        <th class="border p-2" rowspan="2">Subtotal</th>
+                    </tr>
+                    <tr class="bg-gray-200 text-gray-700 uppercase text-sm leading-normal">
+                        <th class="border p-2">Terjual</th>
+                        <th class="border p-2">Retur</th>
+                        <th class="border p-2">Buang</th>
+                    </tr>
+                </thead>
+                <tbody class="text-gray-700 text-sm font-light">
+                    @for ($i = 1; $i < 8; $i++)
+                    <tr class="border-b border-gray-200 hover:bg-gray-100">
+                        <td class="py-3 px-6 text-center">{{ $i }}</td>
+                        <td class="py-3 px-6 text-center">Jenis Packaging Obat</td>
+                        <td class="py-3 px-6 text-center">Margin</td>
+                        <td class="py-3 px-6 text-center">Harga Jual</td>
+                        <td class="py-3 px-6 text-center">Kuantiti</td>
+                        <td class="py-3 px-6 text-center">Terjual</td>
+                        <td class="py-3 px-6 text-center">Retur</td>
+                        <td class="py-3 px-6 text-center">Buang</td>
+                        <td class="py-3 px-6 text-center">Subtotal</td>
+                    </tr>
+                        
+                    @endfor
+                </tbody>
+            </table>
+        </div>
+        <div class="flex justify-end items-center mt-4 gap-4">
+            <div class="text-sm">Showing 1 to 10 of 50 entries</div>
+            <!-- Pagination -->
+            <div class="flex justify-end">
+                <nav class="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-l-md hover:bg-gray-100">1</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">2</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">3</a>
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">...</a>
+                    <a href="#" class="px-3 py-2 border border-gray-300 bg-white text-gray-500 hover:bg-gray-100">9</a>
+                    <a href="#"
+                        class="px-3 py-2 border border-gray-300 bg-white text-gray-500 rounded-r-md hover:bg-gray-100">10</a>
+                </nav>
+            </div>
+        </div>
+    </div>
+
 @endsection
