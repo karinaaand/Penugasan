614641f0e64bfbfc41b72fd34bcedf50b9cae97a
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-10-23 10:19:49 +0700

    [DONE] Minor fixing layout and header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title ?? 'SIMBAT' }}</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     @vite('resources/css/app.css')
 </head>
 
