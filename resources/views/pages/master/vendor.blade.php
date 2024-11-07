9a840a0fbf4badece288739fd9b206a7a363beab
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-11-07 18:09:58 +0700

    Finishing Vendor

--- resources/views/pages/master/vendor.blade.php
+++ resources/views/pages/master/vendor.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 @section('container')
-    <button onclick="showTambahModal()" class="bg-indigo hover:bg-indigo-700 rounded-md p-2 text-white">+ Tambah</button>
+    <button onclick="showTambahModal()" class="bg-green-500 text-white font-semibold px-6 py-2 rounded-lg shadow hover:bg-green-600 transition-colors duration-200">+ Tambah</button>
     <div id="tambahModal" class="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center hidden">
         <div class="bg-white rounded-lg shadow-lg p-8 w-96">
             <h2 class="text-center text-xl font-semibold mb-6">Tambah Vendor</h2>
@@ -18,10 +18,10 @@
                     <label class="block text-gray-700 mb-2" for="address">Alamat</label>
                     <input class="w-full px-3 py-2 border rounded-lg" type="text" id="address" name="address">
                 </div>
-                <div class="flex justify-end space-x-4">
+                <div class="flex justify-center space-x-4 mt-4">
                     <button type="button" id="closeModal" onclick="closeTambahModal()"
-                        class="px-4 py-2 border rounded-lg text-gray-700 border-gray-300">Cancel</button>
-                    <button type="submit" class="px-4 py-2 bg-lime-500 text-white rounded-lg">Tambah</button>
+                        class="px-4 py-2 border rounded-lg text-gray-700 border-gray-300 w-full flex-1">Cancel</button>
+                    <button type="submit" class="px-4 py-2 bg-green-500 text-white rounded-lg w-full flex-1">Tambah</button>
                 </div>
             </form>
         </div>
@@ -41,32 +41,23 @@
             <tbody class="text-gray-700 text-sm font-light">
                 @foreach ($vendors as $number => $item)
                     <tr class="border-b border-gray-200 hover:bg-gray-100">
-                        <td class="py-3 px-6 text-left">{{ $number + 1 }}</td>
+                        <td class="py-3 px-6 text-left">{{ $loop->iteration + ($vendors->currentPage() - 1) * $vendors->perPage() }}</td>
                         <td class="py-3 px-6 text-left">{{ $item->name }}</td>
                         <td class="py-3 px-6 text-left">{{ $item->phone }}</td>
                         <td class="py-3 px-6 text-left">{{ $item->address }}</td>
-                        <td class="py-3 px-6 text-center">
+                        <td class="py-3 px-6 text-center flex justify-center">
                             <a onclick="showEditModal('{{ $item->name }}','{{ $item->address }}','{{ $item->phone }}',{{ $item->id }})"
-                                class="bg-yellow-300 text-white text-sm px-4 py-2 rounded-lg shadow hover:bg-yellow-400 transition-colors duration-200 mr-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M16.586 2.586a2 2 0 00-2.828 0L7.757 9.414a2 2 0 00-.414.586L6 14l4-1.343a2 2 0 00.586-.414l6.829-6.829a2 2 0 000-2.828zM10 12L8 14l1.414-1.414L10 12zm1.586-1.414L16.293 6H14v-2h2.293l-4.707 4.707a1 1 0 00-.586.414z" />
+                                class="flex items-center bg-yellow-300 text-white text-sm px-4 py-2 rounded-lg shadow hover:bg-yellow-400 transition-colors duration-200 mr-2">
+                                <svg width="20" height="21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15.728 9.68602L14.314 8.27202L5 17.586V19H6.414L15.728 9.68602ZM17.142 8.27202L18.556 6.85802L17.142 5.44402L15.728 6.85802L17.142 8.27202ZM7.242 21H3V16.757L16.435 3.32202C16.6225 3.13455 16.8768 3.02924 17.142 3.02924C17.4072 3.02924 17.6615 3.13455 17.849 3.32202L20.678 6.15102C20.8655 6.33855 20.9708 6.59286 20.9708 6.85802C20.9708 7.12319 20.8655 7.37749 20.678 7.56502L7.243 21H7.242Z" fill="black"/>
                                 </svg>
-                                Edit
                             </a>
                             <button type="button" onclick="showDeleteModal({{ $item->id }})"
                                 class="bg-red-500 text-white text-sm px-4 py-2 rounded-lg shadow hover:bg-red-600 transition-colors duration-200">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M6 18L18 6M6 6l12 12" />
+                                <svg width="20" height="21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M14.167 5.50002H18.3337V7.16669H16.667V18C16.667 18.221 16.5792 18.433 16.4229 18.5893C16.2666 18.7456 16.0547 18.8334 15.8337 18.8334H4.16699C3.94598 18.8334 3.73402 18.7456 3.57774 18.5893C3.42146 18.433 3.33366 18.221 3.33366 18V7.16669H1.66699V5.50002H5.83366V3.00002C5.83366 2.77901 5.92146 2.56704 6.07774 2.41076C6.23402 2.25448 6.44598 2.16669 6.66699 2.16669H13.3337C13.5547 2.16669 13.7666 2.25448 13.9229 2.41076C14.0792 2.56704 14.167 2.77901 14.167 3.00002V5.50002ZM15.0003 7.16669H5.00033V17.1667H15.0003V7.16669ZM7.50033 3.83335V5.50002H12.5003V3.83335H7.50033Z" fill="white"/>
                                 </svg>
-                                Delete
                             </button>
-
-
-
                         </td>
                     </tr>
                 @endforeach
@@ -111,10 +102,10 @@
                     <input class="w-full px-3 py-2 border rounded-lg" type="text" id="address" name="address">
                 </div>
 
-                <div class="flex justify-end space-x-4">
+                <div class="flex justify-center space-x-4 mt-4">
                     <button type="button" id="closeModal" onclick="closeEditModal()"
-                        class="px-4 py-2 border rounded-lg text-gray-700 border-gray-300">Cancel</button>
-                    <button type="submit" class="px-4 py-2 bg-lime-500 text-white rounded-lg">Edit</button>
+                        class="px-4 py-2 border rounded-lg text-gray-700 border-gray-300 w-full flex-1">Cancel</button>
+                    <button type="submit" class="px-4 py-2 bg-yellow-500 text-white rounded-lg w-full flex-1">Edit</button>
                 </div>
             </form>
         </div>
