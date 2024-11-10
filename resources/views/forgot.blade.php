7221f89b718bb5a039b15c7e760651e1166464f8
karinaaand <karinaandriani@mail.ugm.ac.id>
2024-11-10 22:29:37 +0700

    gantibahasaforgot

--- resources/views/forgot.blade.php
+++ resources/views/forgot.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.form')
 @section('container')
     <div class="w-full max-w-sm p-6 bg-white rounded-lg shadow-md">
-        <h2 class="mb-6 text-2xl font-semibold text-center text-gray-800">Forgot Password</h2>
+        <h2 class="mb-6 text-2xl font-semibold text-center text-gray-800">Lupa Sandi</h2>
         <form>
             <div class="mb-4">
                 <label class="block mb-2 text-sm font-medium text-gray-700" for="email">
-                    Your email
+                    Email Anda
                 </label>
                 <input
                     class="w-full px-3 py-2 text-sm border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -15,7 +15,7 @@
                 <button
                     class="w-full px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
                     type="submit">
-                    Confirm
+                    Konfirmasi
                 </button>
             </div>
         </form>
