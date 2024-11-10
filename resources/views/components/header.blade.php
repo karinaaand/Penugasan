b46b3314977f24bf5e1b1b20f9a2ed0bd08d949c
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-11-10 07:19:28 +0700

    [DONE] Add back functionality at header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,19 @@
 <div class="flex justify-between items-center p-4">
-    <h1 class="text-2xl font-bold">
-        {{ $judul ?? 'SIMBAT' }}
-    </h1>
+    <div class="flex justify-between items-center">
+        @if (str_contains(request()->route()->getName(),'show') || str_contains(request()->route()->getName(),'create') || str_contains(request()->route()->getName(),'edit'))
+        <a href="{{ url()->previous() }}">
+            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="black" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
+              </svg>
+        </a>
+            
+        @endif
+        <h1 class="text-2xl font-bold">
+            {{ $judul ?? 'SIMBAT' }}
+        </h1>
+
+    </div>
+
     <div class="flex items-center gap-6">
         @if (Request::is('transaction/create'))
             <a href="{{ route('transaction.index') }}" class="bg-bright hover:bg-bright-700 p-2 h-max rounded-xl">
