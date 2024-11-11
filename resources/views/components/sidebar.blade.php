d1eb23500df0c990a4cb77e61d40e5333ef89a0f
karinaaand <karinaandriani@mail.ugm.ac.id>
2024-11-11 14:24:57 +0700

    typosidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -10,7 +10,7 @@
 </button>
 <aside id="sidebar-multi-level-sidebar"
     class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
-    aria-label="Sidebar">~~
+    aria-label="Sidebar">
     <div class="h-full px-3 py-4 overflow-y-auto bg-abu">
         <ul class="space-y-2 font-medium">
             <li>
