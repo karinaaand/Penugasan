fd666457f4b3093160ec56197a1f9ba7b84aebaf
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-11-13 16:56:28 +0700

    perubahan modal

--- resources/views/pages/inventory/addStuff.blade.php
+++ resources/views/pages/inventory/addStuff.blade.php
@@ -2,7 +2,7 @@
 @section('container')
     <div class="rounded-lg bg-white p-6 shadow-lg">
         <div class="justify-right mb-4 flex items-center">
-            <button class="mr-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">Upload</button>
+            <button onclick="uploadModal()" class="mr-4 rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">Upload</button>
             <button class="rounded-lg bg-green-500 px-4 py-2 text-white hover:bg-green-600">Template</button>
         </div>
         <div class="space-y-4">
@@ -28,20 +28,12 @@
                     </div>
                 </div>
             </form>
-            <div class="grid grid-cols-2 gap-4">
+            <div class="grid grid-cols-3 gap-4">
                 <input type="text" id="drugInput" name="drug" class="w-full rounded border border-gray-300 p-2"
                     placeholder="Inputkan nama obat" autocomplete="off">
                 <ul id="suggestions" class="absolute mt-10 border border-gray-300 bg-white rounded hidden"></ul>
 
-                {{-- <input type="text" class="w-full rounded border border-gray-300 p-2" placeholder="Inputkan nama" />
-                <select name="drug" class="w-full rounded border border-gray-300 p-2">
-                    <option selected>Inputkan obat</option>
-                    @foreach ($drugs as $item)
-                        <option value="{{ $item->id }}_{{ $item->name }}_{{ $item->code }}">{{ $item->name }}
-                        </option>
-                    @endforeach
-                </select> --}}
-                <div class="grid grid-cols-3 gap-4">
+                <div class="grid grid-cols-2 gap-4">
                     <div class="flex">
                         <input name="quantity" type="number" class="w-full rounded-s border border-gray-300 p-2"
                             placeholder="Jumlah" />
@@ -53,17 +45,17 @@
                     </div>
                     <input name="price" type="number" class="w-full rounded border border-gray-300 p-2"
                         placeholder="Harga Satuan" />
+                </div>
+                <div class="grid grid-cols-2 gap-8">
                     <div class="flex">
                         <a class="w-full rounded-s border border-gray-300 p-2 bg-gray-200">EXP</a>
                         <input name="expired" type="date" class="w-full rounded-e border border-gray-300 p-2"
                             placeholder="Inputkan expired obat" />
                     </div>
+                    <button onclick="addStuff()" class="rounded-lg bg-purple-500 px-5 py-2 text-white hover:bg-purple-600 text-center">
+                        Tambah
+                    </button>
                 </div>
-            </div>
-            <div class="flex justify-center">
-                <button onclick="addStuff()" class="rounded-lg bg-purple-500 px-20 py-2 text-white hover:bg-purple-600">
-                    Tambah
-                </button>
             </div>
         </div>
     </div>
@@ -71,14 +63,14 @@
         <div class="mt-8">
             <div class="flex justify-between mb-4">
                 <h1>Total: <span id="total" class="font-bold">Rp 0</span></h1>
-                <button onclick="submitForm()" class="rounded-lg bg-green-500 px-12 py-2 text-white hover:bg-green-600 ">
+                <button onclick="submitModal()" class="rounded-lg bg-green-500 px-12 py-2 text-white hover:bg-green-600 ">
                     SAVE
                 </button>
             </div>
             <div class="overflow-hidden rounded-lg bg-white shadow-md">
                 <table class="min-w-full leading-normal">
                     <thead>
-                        <tr class="bg-gray-200 uppercase leading-normal text-black">
+                        <tr class="bg-gray-200 text-sm uppercase leading-normal text-black">
                             <th class="border p-2">Nama Obat</th>
                             <th class="border p-2">Jumlah</th>
                             <th class="border p-2">Harga Satuan</th>
@@ -87,15 +79,95 @@
                             <th class="border p-2">ACTION</th>
                         </tr>
                     </thead>
-                    <tbody class="font-light text-gray-700" id="drugTable">
+                    <tbody class="text-sm font-light text-gray-700" id="drugTable">
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+    <div id="deleteModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
+        <div class="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <button type="button" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600" onclick="closeDeleteModal()">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="text-lg font-semibold text-gray-700 mb-2">Anda yakin untuk menghapus data ini?</h3>
+                <p class="text-sm text-gray-500 mb-5">Tindakan ini tidak dapat dibatalkan.</p>
+            </div>
+            <div class="flex justify-center space-x-4">
+                <button onclick="closeDeleteModal()" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none">
+                    Batal</button>
+                <button onclick="deleteItem()" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 focus:outline-none">
+                    Hapus</button>
+            </div>
+        </div>
+    </div>
+    <div id="saveModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
+        <div class="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <button type="button" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600" onclick="closeSaveModal()">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="text-lg font-semibold text-gray-700 mb-2">Apakah Anda yakin ingin menyimpan transaksi ini?</h3>
+                <p class="text-sm text-gray-500 mb-5">Pastikan semua data sudah benar sebelum menyimpan.</p>
+            </div>
+            <div class="flex justify-center space-x-4">
+                <button onclick="closeSaveModal()" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none">
+                    Batal
+                </button>
+                <button onclick="submitForm()" type="button" class="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 focus:outline-none">
+                    Simpan
+                </button>
+            </div>
+        </div>
+    </div>
+    <div id="uploadModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
+        <div class="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <button type="button" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600" onclick="closeUploadModal()">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="flex items-center justify-center w-full mb-6 mt-6">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 gray:hover:bg-gray-800 gray:bg-gray-700 hover:bg-gray-100 gray:border-gray-600 gray:hover:border-gray-500 gray:hover:bg-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">File format .xls (Max. 10Mb)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" />
+                </label>
+            </div> 
+            <div class="flex justify-center space-x-4">
+                <button onclick="closeUploadModal()" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none">
+                    Batal
+                </button>
+                <button onclick="submitModal()" type="button" class="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 focus:outline-none">
+                    Tambah
+                </button>
+            </div>
+        </div>
+    </div>
+
 @endsection
 
 <script>
+    let deleteForItem = null;
     document.addEventListener('DOMContentLoaded', function() {
         const drugInput = document.querySelector("input[name='drug']")
         const unitInput = document.querySelector("select[name='unit']")
@@ -230,24 +302,46 @@
                             <td>${price}</td>
                             <td>${expired}</td>
                             <td class="py-2">
-                                <button type="button" onclick="deleteItem(${i})"
+                                <button type="button" onclick="showDeleteModal(${i})"
                                     class="bg-red-500 text-white text-sm px-2 py-2 rounded-lg shadow hover:bg-red-600 transition-colors duration-200">
                                     <svg width="20" height="21" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M14.167 5.50002H18.3337V7.16669H16.667V18C16.667 18.221 16.5792 18.433 16.4229 18.5893C16.2666 18.7456 16.0547 18.8334 15.8337 18.8334H4.16699C3.94598 18.8334 3.73402 18.7456 3.57774 18.5893C3.42146 18.433 3.33366 18.221 3.33366 18V7.16669H1.66699V5.50002H5.83366V3.00002C5.83366 2.77901 5.92146 2.56704 6.07774 2.41076C6.23402 2.25448 6.44598 2.16669 6.66699 2.16669H13.3337C13.5547 2.16669 13.7666 2.25448 13.9229 2.41076C14.0792 2.56704 14.167 2.77901 14.167 3.00002V5.50002ZM15.0003 7.16669H5.00033V17.1667H15.0003V7.16669ZM7.50033 3.83335V5.50002H12.5003V3.83335H7.50033Z" fill="white"/>
                                     </svg>
                                 </button>
-                            </button>
                             </td>
                         </tr>`
     }
 
-    function deleteItem(index) {
-        data.splice(index, 1)
+    function showDeleteModal(index) {
+        deleteForItem = index;
+        document.getElementById('deleteModal').classList.remove('hidden');
+    }
+    function submitModal() {
+        document.getElementById('saveModal').classList.remove('hidden');
+    }
+    function closeSaveModal() {
+        document.getElementById('saveModal').classList.add('hidden');
+    }
+
+    function closeDeleteModal() {
+        document.getElementById('deleteModal').classList.add('hidden');
+    }
+    function uploadModal() {
+        document.getElementById('uploadModal').classList.remove('hidden');
+    }
+    function closeUploadModal() {
+        document.getElementById('uploadModal').classList.add('hidden');
+    }
+
+
+
+    function deleteItem() {
+        closeDeleteModal()
+        data.splice(deleteForItem, 1)
         draw()
     }
 
     function submitForm() {
-        if (confirm("Apakah Anda yakin ingin menyimpan semua data?")) {
             data = data.map(function(e) {
                 return {
                     name: e[0],
@@ -262,7 +356,5 @@
             document.querySelector("input[name='transaction']").value = JSON.stringify(data)
             // document.querySelector("input[name='transaction']").value = JSON.stringify(data).replaceAll('{','[').replaceAll('}',']').replaceAll(':','=>')
             document.querySelector("form").submit()
-            showToast("Data berhasil disimpan!");
-        }
     }
 </script>
--- resources/views/pages/inventory/inflowDetail.blade.php
+++ resources/views/pages/inventory/inflowDetail.blade.php
@@ -3,41 +3,47 @@
 @endphp
 @extends('layouts.main')
 @section('container')
+<div class="mb-4 flex items-center">
+            <!-- <div id="printOptions" class="mb-4 invisible ml-5">
+                <label for="format" class="text-lg font-semibold">Pilih format cetak:</label>
+                <select id="format" class="ml-2 rounded-md border">
+                    <option value="pdf">PDF</option>
+                    <option value="excel">Excel</option>
+                </select>
+                <button id="confirmPrint" class="ml-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white">
+                    Download
+                </button>
+            </div> -->
+            <div class="flex flex-1 justify-end mr-5">
+                <button onclick="uploadModal()" class="rounded-lg bg-yellow-400 px-4 py-2 font-bold text-black">CETAK</button>
+            </div>
+        </div>
     <div class="rounded-lg bg-white p-6 shadow-lg">
         <div class="mb-4 flex items-center">
-            <div class="flex-1">
-                <h2 class="text-2xl font-bold">{{ $transaction->vendor()->name }}</h2>
-                <p class="text-gray-600">Tanggal : {{ Carbon::parse($transaction->created_at)->translatedFormat('j F Y') }}</p>
+            <div class="flex-1 mr-5">
+                <h2 class="text-2xl font-bold text-left">Klinik Dokter hewan Hendrik</h2>
+                <h2 class="text text-left">Pakuwon Asri, Jl. Sadewa No.3, Karangtengah Lor, Kaliwungu, Kendal, Jawa Tengah 51372</h2>
+                <h2 class="text text-left">085290078739</h2>
             </div>
-            <div class="flex flex-1 items-center justify-center">
-                <label for="lpb" class="mr-2 text-lg font-normal text-black">No. LPB :</label>
-                <input
-                disabled
-                value="{{ $transaction->code }}"
-                    type="text"
-                    id="lpb"
-                    class="mr-2 border-b border-gray-300 focus:border-gray-500 focus:outline-none"
-                />
+            <div class="flex-[1.5] flex flex-col  items-center justify-center">
+                <div class="flex items-center">
+                    <span class="mr-2 text-lg font-normal text-black">No. LPB :</span>
+                    <span class="text-lg font-normal">{{ $transaction->code }}</span>
+                </div>
+                <h1 class="text-center text-3xl font-bold">LAPORAN PENERIMAAN BARANG</h1>
+
+                <!-- <p class="text-gray-600">Tanggal : {{ Carbon::parse($transaction->created_at)->translatedFormat('j F Y') }}</p> -->
             </div>
-            <div class="flex flex-1 justify-end">
-                <button id="printButton" class="rounded-lg bg-yellow-400 px-4 py-2 font-bold text-black">CETAK</button>
+            <div class="flex-1 ml-10">
+                <p class="text-lg mb-4 text-left">Tanggal: {{ Carbon::parse($transaction->created_at)->translatedFormat('j F Y') }}</p>
+                <h2 class="text-2xl font-bold text-left">{{ $transaction->vendor()->name }}</h2>
+                <h2 class="text-lg text-left">{{ $transaction->vendor()->address }}</h2>
+                <h2 class="text-lg text-left">{{ $transaction->vendor()->phone }}</h2>
             </div>
-        </div>
-        <div id="printOptions" class="mb-4 hidden">
-            <label for="format" class="text-lg font-semibold">Pilih format cetak:</label>
-            <select id="format" class="ml-2 rounded-md border">
-                <option value="pdf">PDF</option>
-                <option value="excel">Excel</option>
-            </select>
-            <button id="confirmPrint" class="ml-2 rounded-lg bg-green-500 px-4 py-2 font-bold text-white">
-                Download
-            </button>
         </div>
 
-        <h1 class="text-center text-3xl font-bold">LAPORAN PENERIMAAN BARANG</h1>
-        <p class="mb-2 font-semibold">Diterima</p>
         <!-- Table -->
-        <div class="overflow-hidden rounded-lg bg-white shadow-md">
+        <div class="overflow-hidden rounded-lg bg-white shadow-md mt-8">
             <table class="min-w-full leading-normal">
                 <thead>
                     <tr class="bg-gray-200 text-sm uppercase leading-normal text-black">
@@ -54,7 +60,7 @@
                     <tr class="border-b border-gray-200 hover:bg-gray-100">
                         <td class="px-6 py-3 text-center">{{ $number+1 }}</td>
                         <td class="px-6 py-3 text-center">{{ $item->drug()->code }}</td>
-                        <td class="px-6 py-3 text-center">{{ $item->drug()->name }}</td>
+                        <td class="px-6 py-3 text-left">{{ $item->drug()->name }}</td>
                         <td class="px-6 py-3 text-center">{{ $item->quantity }}</td>
                         <td class="px-6 py-3 text-center">{{ 'Rp ' . number_format($item->piece_price, 0, ',', '.') }}</td>
                         <td class="px-6 py-3 text-center">{{ 'Rp ' . number_format($item->total_price, 0, ',', '.') }}</td>
@@ -63,25 +69,53 @@
                     @endforeach
                 </tbody>
             </table>
-        </div>
-        <div class="mt-4 flex items-center justify-end">
-            <p class="mr-2 font-semibold">Grand total :</p>
-            <input type="text" value="{{ 'Rp ' . number_format($transaction->outcome, 0, ',', '.') }}"
-                class="w-48 border-b border-gray-400 text-center focus:border-black focus:outline-none" readonly />
+            <h2 type="text" class="text-right mt-6 pe-6 pb-6">Grand total : {{ 'Rp ' . number_format($transaction->outcome, 0, ',', '.') }}</h2>
         </div>
     </div>
+    <div id="uploadModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
+        <div class="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <button type="button" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600" onclick="closeUploadModal()">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="text-lg font-semibold text-gray-700 mb-2">Apa format file yang ingin Anda simpan?</h3>
+                <p class="text-sm text-gray-500 mb-5">Pilihlah salah satu format file!</p>
+            </div>
+            <div class="flex justify-center space-x-4">
+                <button onclick="closeUploadModal()" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-green-500 focus:outline-none">
+                    Excel
+                </button>
+                <button onclick="submitModal()" type="button" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-green-500 focus:outline-none">
+                    PDF
+                </button>
+            </div>
+        </div>
+    </div>
+    
     <script>
-        document.getElementById('printButton').onclick = function() {
-            document.getElementById('printOptions').classList.toggle('hidden');
+            function uploadModal() {
+            document.getElementById('uploadModal').classList.remove('hidden');
+        }
+        function closeUploadModal() {
+            document.getElementById('uploadModal').classList.add('hidden');
+        }
+        document.getElementById('printButton').onclick = function () {
+            document.getElementById('printOptions').classList.toggle('invisible');
         };
-        document.getElementById('confirmPrint').onclick = function() {
+        document.getElementById('confirmPrint').onclick = function () {
             const format = document.getElementById('format').value;
             if (format === 'pdf') {
                 alert('Mencetak dalam format PDF...');
             } else if (format === 'excel') {
                 alert('Mencetak dalam format Excel...');
             }
-            document.getElementById('printOptions').classList.add('hidden');
+            document.getElementById('printOptions').classList.add('invisible');
         };
     </script>
 @endsection
