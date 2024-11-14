e9a3655f6aca5bfc6ddfc261e9963881b68e595a
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-11-14 13:24:46 +0700

    [EDITED] perubahan user modal

--- resources/views/pages/super/user.blade.php
+++ resources/views/pages/super/user.blade.php
@@ -59,14 +59,25 @@
                         </button>
                     <div class="text-center">
                         <h2 class="text-center text-xl font-semibold mb-6">Tambah Akun</h2>
-                        <div class="relative mb-4">
+                            <div class="flex items-center mb-4">
+                                <div class="w-30 h-30 rounded-full bg-gray-700 overflow-hidden">
+                                    <img src="https://via.placeholder.com/64" alt="Avatar" class="object-cover w-full h-full">
+                                </div>
+                                <div class="ml-4">
+                                    <button onclick="uploadModal()" class="bg-blue-600 text-sm text-white px-4 py-2 rounded-lg hover:bg-blue-500">
+                                        Pilih gambar
+                                    </button>
+                                    <p class="text-gray-400 text-xs mt-1">JPG, GIF or PNG. 1MB max.</p>
+                                </div>
+                            </div>
+                            <div class="relative mb-4">
                                 <label class="mb-2 block text-md font-medium text-left">Nama</label>
-                                <input type="text" class="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan nama"/>
+                                <input type="text" class="bg-gray-100 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan nama"/>
                             </div>
                             <div class="relative mb-4">
                                 <label class="mb-2 block text-md font-medium text-left">Role</label>
-                                <select class="w-full rounded border border-gray-300 p-3">
-                                    <option selected disabled>Masukkan Role</option>
+                                <select class="bg-gray-100 w-full rounded border border-gray-300 p-3">
+                                    <option selected disabled te>Masukkan Role</option>
                                     <option>Super Admin</option>
                                     <option>SIMKLINIK</option>
                                     <option>Dokter</option>
@@ -75,11 +86,11 @@
                             </div>
                             <div class="mb-6">
                                 <label class="mb-2 block text-md font-medium text-left">Email</label>
-                                <input type="text" class="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan email"/>
+                                <input type="text" class="bg-gray-100 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan email"/>
                             </div>
                             <div class="mb-6">
                                 <label class="mb-2 block text-md font-medium text-left">Kata sandi</label>
-                                <input type="password" class="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan kata sandi"/>
+                                <input type="password" class="bg-gray-100 w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan kata sandi"/>
                             </div>
                     </div>
                     <div class="flex justify-center space-x-4">
@@ -106,7 +117,18 @@
                     </button>
                 <div class="text-center">
                     <h2 class="text-center text-xl font-semibold mb-6">Ubah Akun</h2>
-                    <div class="relative mb-4">
+                    <div class="flex items-center mb-4">
+                        <div class="w-30 h-30 rounded-full bg-gray-700 overflow-hidden">
+                                <img src="https://via.placeholder.com/64" alt="Avatar" class="object-cover w-full h-full">
+                            </div>
+                            <div class="ml-4">
+                                <button onclick="uploadModal()" class="bg-blue-600 text-sm text-white px-4 py-2 rounded-lg hover:bg-blue-500">
+                                    Pilih gambar
+                                </button>
+                                <p class="text-gray-400 text-xs mt-1">JPG, GIF or PNG. 1MB max.</p>
+                            </div>
+                        </div>
+                        <div class="relative mb-4">
                             <label class="mb-2 block text-md font-medium text-left">Nama</label>
                             <input type="text" class="w-full rounded-lg border px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500" placeholder="Masukkan nama"/>
                         </div>
@@ -156,6 +178,36 @@
                     Batal</button>
                 <button onclick="deleteItem()" class="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 focus:outline-none">
                     Hapus</button>
+            </div>
+        </div>
+    </div>
+    <div id="uploadModal" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 hidden">
+        <div class="bg-white rounded-lg shadow-lg p-6 w-96 relative">
+            <button type="button" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600" onclick="closeUploadModal()">
+                <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1l6 6m0 0l6 6M7 7l6-6M7 7L1 13"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="flex items-center justify-center w-full mb-6 mt-6">
+                <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 gray:hover:bg-gray-800 gray:bg-gray-700 hover:bg-gray-100 gray:border-gray-600 gray:hover:border-gray-500 gray:hover:bg-gray-600">
+                    <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                        </svg>
+                        <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+                        <p class="text-xs text-gray-500 dark:text-gray-400">File format .xls (Max. 10Mb)</p>
+                    </div>
+                    <input id="dropzone-file" type="file" class="hidden" />
+                </label>
+            </div> 
+            <div class="flex justify-center space-x-4">
+                <button onclick="closeUploadModal()" class="px-4 py-2 bg-gray-200 text-gray-700 rounded-lg hover:bg-gray-300 focus:outline-none">
+                    Batal
+                </button>
+                <button onclick="submitModal()" type="button" class="px-4 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 focus:outline-none">
+                    Tambah
+                </button>
             </div>
         </div>
     </div>
@@ -232,6 +284,12 @@
 
     function closeSubmitEdirForm() {
         document.getElementById('saveModal').classList.add('hidden');
+    }
+    function uploadModal() {
+        document.getElementById('uploadModal').classList.remove('hidden');
+    }
+    function closeUploadModal() {
+        document.getElementById('uploadModal').classList.add('hidden');
     }
         
 </script>
