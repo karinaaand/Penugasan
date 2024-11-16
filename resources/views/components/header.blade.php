6dd327a9b918bab830ce0147e0f719dfa2e83a6e
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-11-16 11:41:10 +0700

    [EDITED] Ganti Warna co di header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -63,7 +63,7 @@
                 </div>
                 <div class="mt-4 flex justify-center">
                     <!-- Icon Setting -->
-                    <a href="{{ route('user.settings') }}" class="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text-center">
+                    <a href="{{ route('user.settings') }}" class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text-center">
                         <svg class="mr-2"  fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                             viewBox="0 0 478.703 478.703" xml:space="preserve">
                             <g>
@@ -96,7 +96,7 @@
                 </div>
                 <div class="mt-10 flex justify-center">
                     <!-- Icon Logout -->
-                    <a href="{{ route('user.logout') }}" class="bg-gray-300 text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text text-center">
+                    <a href="{{ route('user.logout') }}" class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text text-center">
                         <svg class="mr-2" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.2429 22 18.8286 22 16.0002 22H15.0002C12.1718 22 10.7576 22 9.87889 21.1213C9.11051 20.3529 9.01406 19.175 9.00195 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round"/>
                             <path d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
