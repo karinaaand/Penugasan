72a37125c3da9e55c795e74510c0543b13eb1d32
dimalahmad <dimalkarimahmad@mail.ugm.ac.id>
2024-11-16 09:20:43 +0700

    [EDITED]standarisasi bahasa dan warna

--- resources/views/pages/master/editDrug.blade.php
+++ resources/views/pages/master/editDrug.blade.php
@@ -185,7 +185,7 @@
                             %
                         </span>
                     </div>
-                    <button class="bg-green-500 text-white rounded hover:bg-green-600 px-4 py-2 w-28">Tambah</button>
+                    <button class="bg-blue-500 text-white rounded hover:bg-blue-700 px-4 py-2 w-28">Tambah</button>
                 </div>
             </form>
             <div class="bg-white shadow-md rounded-lg overflow-hidden">
