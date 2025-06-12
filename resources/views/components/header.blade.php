5725d4a23e3bf46f1def284a6efe1b7bcea975ef
Karina Andriani <karinaandriani@mail.ugm.ac.id>
2025-06-12 22:01:02 +0700

    konsum api beberapa

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,16 @@
 <div class="flex justify-between items-center p-4">
     <div class="flex justify-between items-center">
-        @if (str_contains(request()->route()->getName(),'show') || str_contains(request()->route()->getName(),'create') || str_contains(request()->route()->getName(),'edit') ||  str_contains(request()->route()->getName(),'index'))
-        <a href="{{ url()->previous() }}">
-            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="black" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5"/>
-              </svg>
-        </a>
-
+        @if (str_contains(request()->route()->getName(), 'show') ||
+                str_contains(request()->route()->getName(), 'create') ||
+                str_contains(request()->route()->getName(), 'edit') ||
+                str_contains(request()->route()->getName(), 'index'))
+            <a href="{{ url()->previous() }}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="black"
+                    class="bi bi-arrow-left-short" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5" />
+                </svg>
+            </a>
         @endif
         <h1 class="text-2xl font-bold">
             {{ $judul ?? 'SIMBAT' }}
@@ -16,7 +20,8 @@
 
     <div class="flex items-center gap-3">
         @if (Request::is('transaction/create'))
-            <a href="{{ route('transaction.index') }}" class="bg-yellow-400 hover:bg-yellow-600 px-6 py-2 h-max text-white rounded-xl">
+            <a href="{{ route('transaction.index') }}"
+                class="bg-yellow-400 hover:bg-yellow-600 px-6 py-2 h-max text-white rounded-xl">
                 History
 
             </a>
@@ -25,56 +30,70 @@
             <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                     d="M2 14V2H0V0H3C3.26522 0 3.51957 0.105357 3.70711 0.292893C3.89464 0.48043 4 0.734784 4 1V13H16.438L18.438 5H6V3H19.72C19.872 3 20.022 3.03466 20.1586 3.10134C20.2952 3.16801 20.4148 3.26495 20.5083 3.38479C20.6019 3.50462 20.6668 3.6442 20.6983 3.79291C20.7298 3.94162 20.7269 4.09555 20.69 4.243L18.19 14.243C18.1358 14.4592 18.011 14.6512 17.8352 14.7883C17.6595 14.9255 17.4429 15 17.22 15H3C2.73478 15 2.48043 14.8946 2.29289 14.7071C2.10536 14.5196 2 14.2652 2 14ZM4 21C3.46957 21 2.96086 20.7893 2.58579 20.4142C2.21071 20.0391 2 19.5304 2 19C2 18.4696 2.21071 17.9609 2.58579 17.5858C2.96086 17.2107 3.46957 17 4 17C4.53043 17 5.03914 17.2107 5.41421 17.5858C5.78929 17.9609 6 18.4696 6 19C6 19.5304 5.78929 20.0391 5.41421 20.4142C5.03914 20.7893 4.53043 21 4 21ZM16 21C15.4696 21 14.9609 20.7893 14.5858 20.4142C14.2107 20.0391 14 19.5304 14 19C14 18.4696 14.2107 17.9609 14.5858 17.5858C14.9609 17.2107 15.4696 17 16 17C16.5304 17 17.0391 17.2107 17.4142 17.5858C17.7893 17.9609 18 18.4696 18 19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21Z"
-                    fill="white"/>
+                    fill="white" />
             </svg>
         </a>
-<div class="relative inline-block text-left">
-    <button onclick="toggleModal()" class="flex items-center justify-center w-10 h-10 bg-white border-none rounded-full focus:outline-none">
-        <img src="{{ Storage::url(auth()->user()->avatar) }}" alt="Avatar" class="w-10 h-10 rounded-full">
-    </button>
-</div>
+        {{--  <div class="relative inline-block text-left">
+            <button onclick="toggleModal()" class="flex items-center justify-center w-10 h-10 bg-white border-none rounded-full focus:outline-none">
+                <img id="user-avatar" src="" alt="Avatar" class="w-10 h-10 rounded-full">
+            </button>
+        </div>  --}}
+
+        <div class="relative inline-block text-left">
+            <button onclick="toggleModal()"
+                class="flex items-center justify-center w-10 h-10 bg-white border-none rounded-full focus:outline-none">
+                <img id="user-avatar" src="" alt="Avatar" class="w-10 h-10 rounded-full">
+            </button>
+        </div>
+
         <div id="modal" class="fixed inset-0 hidden bg-black bg-opacity-50 z-50">
             <div class="bg-white w-72 rounded-lg shadow-lg p-6 absolute top-4 right-4 text-left">
-                <button onclick="toggleModal()" class="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                <button onclick="toggleModal()"
+                    class="absolute top-2 right-2 text-gray-500 hover:text-gray-700 focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
-               <div class="relative w-16 h-16 bg-gray-200 rounded-full flex justify-start">
-                    <img src="{{ Storage::url(auth()->user()->avatar) }}" alt="Avatar" class="rounded-full w-full h-full">
-                    <span class="absolute bottom-0 right-0 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></span>
+                <div class="relative w-16 h-16 bg-gray-200 rounded-full flex justify-start">
+                    {{--  <img src="" alt="Avatar" class="rounded-full w-full h-full">  --}}
+                    <img id="user-avatar-modal" src="" alt="Avatar" class="rounded-full w-full h-full">
+                    <span
+                        class="absolute bottom-0 right-0 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></span>
                 </div>
                 <div class="mt-4 w-full">
                     <div class="flex items-center text-sm text-gray-600 font-semibold mb-2">
                         <span class="w-20 text-left flex-shrink-0">Nama</span>
                         <span class="">:</span>
-                        <span class="ml-1">{{ auth()->user()->name }}</span>
+                        <span id="sp-name" class="ml-1"></span>
                     </div>
                     <div class="flex items-center text-sm text-gray-600 font-semibold mb-2">
                         <span class="w-20 text-left flex-shrink-0">Role</span>
                         <span class="">:</span>
-                        <span class="ml-1">{{ auth()->user()->role }}</span>
+                        <span id="sp-role" class="ml-1"></span>
                     </div>
                     <div class="flex items-center text-sm text-gray-600 font-semibold mb-4">
                         <span class="w-20 text-left flex-shrink-0">Email</span>
                         <span class="">:</span>
-                        <span class="ml-1">{{ substr(auth()->user()->email,0,15) }}
-                            @if (strlen(auth()->user()->email)>15)
-                                {{ "***" }}
-                            @endif
-                        </span>
+                        <span id="sp-email" class="ml-1"></span>
                     </div>
                 </div>
                 <div class="mt-4 flex justify-center">
-                    @if (auth()->user()->role=="super")
 
 
-                    <a href="{{ route('user.settings') }}" class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text-center">
-                        <svg class="mr-2"  fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                    {{--  @if (auth()->user()->role == 'super')  --}}
+
+
+                    <a href="{{ route('user.settings') }}"
+                        class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text-center">
+                        <svg class="mr-2" fill="#000000" height="20px" width="20px" version="1.1" id="Capa_1"
+                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                             viewBox="0 0 478.703 478.703" xml:space="preserve">
                             <g>
                                 <g>
-                                    <path d="M454.2,189.101l-33.6-5.7c-3.5-11.3-8-22.2-13.5-32.6l19.8-27.7c8.4-11.8,7.1-27.9-3.2-38.1l-29.8-29.8 c-5.6-5.6-13-8.7-20.9-8.7c-6.2,0-12.1,1.9-17.1,5.5l-27.8,19.8c-10.8-5.7-22.1-10.4-33.8-13.9l-5.6-33.2 c-2.4-14.3-14.7-24.7-29.2-24.7h-42.1c-14.5,0-26.8,10.4-29.2,24.7l-5.8,34c-11.2,3.5-22.1,8.1-32.5,13.7l-27.5-19.8
+                                    <path
+                                        d="M454.2,189.101l-33.6-5.7c-3.5-11.3-8-22.2-13.5-32.6l19.8-27.7c8.4-11.8,7.1-27.9-3.2-38.1l-29.8-29.8 c-5.6-5.6-13-8.7-20.9-8.7c-6.2,0-12.1,1.9-17.1,5.5l-27.8,19.8c-10.8-5.7-22.1-10.4-33.8-13.9l-5.6-33.2 c-2.4-14.3-14.7-24.7-29.2-24.7h-42.1c-14.5,0-26.8,10.4-29.2,24.7l-5.8,34c-11.2,3.5-22.1,8.1-32.5,13.7l-27.5-19.8
                                         c-5-3.6-11-5.5-17.2-5.5c-7.9,0-15.4,3.1-20.9,8.7l-29.9,29.8c-10.2,10.2-11.6,26.3-3.2,38.1l20,28.1
                                         c-5.5,10.5-9.9,21.4-13.3,32.7l-33.2,5.6c-14.3,2.4-24.7,14.7-24.7,29.2v42.1c0,14.5,10.4,26.8,24.7,29.2l34,5.8
                                         c3.5,11.2,8.1,22.1,13.7,32.5l-19.7,27.4c-8.4,11.8-7.1,27.9,3.2,38.1l29.8,29.8c5.6,5.6,13,8.7,20.9,8.7c6.2,0,12.1-1.9,17.1-5.5
@@ -92,23 +111,28 @@
                                         c4.4,3.1,10.2,3.3,14.8,0.6c13-7.8,27.1-13.8,41.8-17.6c5.1-1.4,9-5.6,9.9-10.8l7.2-42.3c0.2-1.3,1.3-2.2,2.6-2.2h42.1
                                         c1.3,0,2.4,0.9,2.6,2.2l7,41.7c0.9,5.3,4.8,9.6,10,10.9c15.1,3.8,29.5,9.7,42.9,17.6c4.6,2.7,10.3,2.5,14.7-0.6l34.5-24.8
                                         c0.5-0.3,1-0.5,1.5-0.5c0.4,0,1.2,0.1,1.9,0.8l29.8,29.8c0.9,0.9,1,2.3,0.3,3.4l-24.7,34.7c-3.1,4.3-3.3,10.1-0.6,14.7
-                                        c7.8,13.1,13.6,27.2,17.4,41.9c1.3,5.2,5.6,9.1,10.8,9.9l42,7.1c1.3,0.2,2.2,1.3,2.2,2.6v42.1H451.9z"/>
-                                    <path d="M239.4,136.001c-57,0-103.3,46.3-103.3,103.3s46.3,103.3,103.3,103.3s103.3-46.3,103.3-103.3S296.4,136.001,239.4,136.001
-                                        z M239.4,315.601c-42.1,0-76.3-34.2-76.3-76.3s34.2-76.3,76.3-76.3s76.3,34.2,76.3,76.3S281.5,315.601,239.4,315.601z"/>
+                                        c7.8,13.1,13.6,27.2,17.4,41.9c1.3,5.2,5.6,9.1,10.8,9.9l42,7.1c1.3,0.2,2.2,1.3,2.2,2.6v42.1H451.9z" />
+                                    <path
+                                        d="M239.4,136.001c-57,0-103.3,46.3-103.3,103.3s46.3,103.3,103.3,103.3s103.3-46.3,103.3-103.3S296.4,136.001,239.4,136.001
+                                        z M239.4,315.601c-42.1,0-76.3-34.2-76.3-76.3s34.2-76.3,76.3-76.3s76.3,34.2,76.3,76.3S281.5,315.601,239.4,315.601z" />
                                 </g>
                             </g>
                         </svg>
-                    Atur Profile Klinik</a>
-                    @endif
+                        Atur Profile Klinik</a>
+                    {{--  @endif  --}}
                 </div>
                 <div class="mt-10 flex justify-center">
                     <!-- Icon Logout -->
-                    <a href="{{ route('user.logout') }}" class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text text-center">
-                        <svg class="mr-2" width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.2429 22 18.8286 22 16.0002 22H15.0002C12.1718 22 10.7576 22 9.87889 21.1213C9.11051 20.3529 9.01406 19.175 9.00195 17" stroke="#000000" stroke-width="1.5" stroke-linecap="round"/>
-                            <path d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        Keluar</a>
+                        <button id="logout" class="bg-gray-300 hover:bg-blue-500 hover:text-white text-gray-700 py-2 px-4 rounded-lg flex items-center justify-center w-full text text-center">
+                        <svg class="mr-2" width="20px" height="20px" viewBox="0 0 24 24" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.2429 22 18.8286 22 16.0002 22H15.0002C12.1718 22 10.7576 22 9.87889 21.1213C9.11051 20.3529 9.01406 19.175 9.00195 17"
+                                stroke="#000000" stroke-width="1.5" stroke-linecap="round" />
+                            <path d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" stroke="#000000" stroke-width="1.5"
+                                stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                        Keluar</button>
                 </div>
             </div>
         </div>
@@ -120,5 +144,32 @@
         const modal = document.getElementById('modal');
         modal.classList.toggle('hidden');
     }
-</script>
+    {{--  const user = JSON.parse(localStorage.getItem('user'));  --}}
+    console.log(user.avatar);
+    {{--  document.getElementById('user-avatar').src = user.avatar;  --}}
+    document.getElementById('user-avatar').src = 'http://localhost:8000/storage/' + user.avatar;
+    document.getElementById('user-avatar-modal').src = 'http://localhost:8000/storage/' + user.avatar;
+    document.getElementById('sp-name').innerText = user.name;
+    document.getElementById('sp-role').innerText = user.role;
+    document.getElementById('sp-email').innerText = user.email;
 
+    if (user.role === 'super') {
+        document.getElementById('list-masterdata').classList.remove('display-none');
+    }
+
+     document.getElementById('logout').addEventListener('click', async () => {
+        try {
+            await axios.post('http://localhost:8000/api/v1/logout', {}, {
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('token')
+                }
+            });
+
+            // Hapus token, redirect ke login
+            localStorage.removeItem('token');
+            window.location.href = '/login';
+        } catch (err) {
+            alert('Gagal logout');
+        }
+    });
+</script>
